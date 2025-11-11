477c354bf366bf281bf9761f4c994b17e35015b2
steamproof <93405617+steamproof@users.noreply.github.com>
2025-11-10 17:49:58 -0800

    lesson 6

---
+++ lesson6/classwork/practice.js
@@ -0,0 +1,22 @@
+// Problem 1
+// Count and print how many times "dog" appears in the list.
+
+
+
+// Problem 2
+// Count and print how many numbers are odd in the list (a number is odd if it's not divisible by 2).
+
+
+
+// Problem 3
+// Search for "monkey" in the list and print its index if it's found.
+
+
+
+// Problem 4
+// Search for 99 in the list and print if itâ€™s found.
+
+
+
+// Problem 5
+// Count and print how many numbers are even in the list (a number is odd if it's divisible by 2).
---
+++ lesson6/homework/problems.js
@@ -0,0 +1,22 @@
+// Problem 1
+// Count and print how many times "Alex" appears in the list.
+
+
+
+// Problem 2
+// Search for "elephant" in the list and print if it's found.
+
+
+
+// Problem 3
+// Count and print how many scores are 100.
+
+
+
+// Problem 4
+// Search for the color "blue" in the list and print its index if it's found.
+
+
+
+// Problem 5
+// Count and print how many temperatures in the list are below zero.

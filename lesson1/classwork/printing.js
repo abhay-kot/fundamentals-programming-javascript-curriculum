6683fc75f03a4df192014aa61a6bc4e53cc7d394
abhay-kot <abhay.kotch@gmail.com>
2026-03-23 00:58:15 +0000

    lesson 1

---
+++ lesson1/classwork/printing.js
@@ -0,0 +1,6 @@
+console.log("Hello, world!");
+console.log("My name is Abhay");
+console.log('My favorite food is ice cream');
+
+// Error: quatation marks mismatched
+// console.log('My favorite food is ice cream");
---
+++ lesson1/classwork/variables.js
@@ -0,0 +1,10 @@
+let x = 5;  // number: Whole-number value
+console.log(x);
+
+let number = 3.14;
+console.log(number);
+
+let favoritefood = "pizza"; //string: text
+console.log("My favorite food is " + favoritefood);
+
+console.log(3.18 + " dogs " + x); // string concatenaion

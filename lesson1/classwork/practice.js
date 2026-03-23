9406ce18c9a03603b8b967f7b8ac5b3cf4daa5df
abhay-kot <abhay.kotch@gmail.com>
2026-03-23 23:57:58 +0000

    classwork practice

--- lesson1/classwork/practice.js
+++ lesson1/classwork/practice.js
@@ -1,24 +1,35 @@
 // Problem 1
 // Create a variable for your favorite number and print it.
+let number = 67; 
 
+console.log("My favorite number is " + number);
 
 
 // Problem 2
-// Create a variable for your height in meters and print it.
+// Create a variable for your height in feet and print it.
+let height = 4
 
-
+console.log("My height is "+height+"feet " );
 
 // Problem 3
 // Create a variable for your favorite animal and print it in a full sentence.
+let animal = "dog" 
 
-
-
+console.log("My favorite animal is "+animal)
 // Problem 4
 // Create two variables: one for your first name and one for your last name.
 // Print them on 2 separate lines.
+let first = "Abhay";
+let last = "kotcharlakota";
 
+console.log("My fist name is"+first)
 
+console.log("My last name is"+last) 
 
 // Problem 5
 // Create 3 variables: your first name, your age, and your favorite color.
 // Print them on the same line in a full sentence.
+let Create = "Abhay" 
+let age = 9
+let color = "blue"
+console.log("My name is "+Create+" My age is "+age+" My favorite color is "+color); 

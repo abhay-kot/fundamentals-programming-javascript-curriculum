a3dbd953e6425f2a5ae94225fe4653d4c473e268
abhay-kot <abhay.kotch@gmail.com>
2026-03-25 00:20:20 +0000

    Lession 1 homework

--- lesson1/homework/problems.js
+++ lesson1/homework/problems.js
@@ -1,24 +1,31 @@
 // Problem 1
 // Create a variable for your lucky number and print it.
-
-
+let lucky = 7;
+console.log ("My lucky number is "+lucky);
 
 // Problem 2
 // Create a variable for the number of hours you sleep and print it.
-
+let sleep = 12;
+console.log("I sleep for "+sleep+ " hours " );
 
 
 // Problem 3
 // Create a variable for your favorite fruit and print it in a full sentence.
-
-
+let fruit = "mango";
+console.log ("My favorite fruit is "+fruit);
 
 // Problem 4
 // Create two variables: one for your city and one for your country.
 // Print them on 2 separate lines.
-
-
+let city = "Seatle";
+let country = "America";
+console.log("The city I live in is "+city);
+console.log("The country I live in is "+country);
 
 // Problem 5
 // Create 3 variables: your pet's name, its age, and its type.
 // Print them on the same line in a full sentence.
+let pet = "shrimpy";
+let age = "1 year";
+let type = "cherry shrimp";
+console.log("My pets name is " + pet+". My pets age is "+age+". My pets type is a "+type );

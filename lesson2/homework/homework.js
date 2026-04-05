e3c5616f3e7fa7e47ebde1be4a29469c8f2b19b8
abhay-kot <abhay.kotch@gmail.com>
2026-04-05 22:44:43 +0000

    homework.js

--- lesson2/homework/homework.js
+++ lesson2/homework/homework.js
@@ -1,14 +1,20 @@
 // Homework Problem 1
 // Ask the user for two numbers.
 // Print their quotient and remainder on separate lines.
-
+const prompt = require("prompt-sync")();
+let first = prompt("Can you tell me the first number?");
+let second = prompt ("Can you tell me the second number?")
+let quo = parseInt (first/second)
+let rem = first-(quo * second);
+console.log("The remander is "+rem+ ".The quotiet is "+quo) 
 
 
 // Homework Problem 2
 // Ask the user for their favorite animal and favorite color.
 // Print a sentence combining them like: "A blue tiger would be awesome!"
-
-
+let ani = prompt ("Can you tell me what your favorite animal is? ")
+let color = prompt ("What is your favorite color? ")
+console.log("A  "  +  color    + " "    +        ani +  " would be very cool.  " )
 
 // Homework Problem 3
 // Use a for loop to print all the even numbers from 0 to 10 (including 10).

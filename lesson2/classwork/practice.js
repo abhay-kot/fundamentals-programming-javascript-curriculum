ba057e85e3027e80440b4a0ad5b192df6fc99721
abhay-kot <abhay.kotch@gmail.com>
2026-04-05 21:08:41 +0000

    classword

--- lesson2/classwork/practice.js
+++ lesson2/classwork/practice.js
@@ -1,15 +1,24 @@
 // Problem 1
 // Ask the user for their age.
 // Calculate and print how many decades old they are, rounded to the nearest whole number.
+console.log(5+15)//20
+console.log("5"+"15")//515
+console.log("5"+15)//515
+console.log(5+"15")//515
 
 
+const prompt = require ("prompt-sync")();
 
+let age = prompt ("How old are you ")
+let decades = parseInt(age/10)
+console.log("You are "+ decades +" decades old")
 // Problem 2
 // Ask the user to enter a number.
 // Print the result of multiplying that number by 5.
 
 
-
+let number = prompt ("What number do you want to x by 5")
+console.log ("Your number is " + 5 * number)
 // Problem 3
 // Use a for loop to print "I will learn JavaScript!" 3 times.
 
@@ -18,7 +27,7 @@
 // Problem 4
 // Ask the user for their name and age.
 // Print their name and how old they will be one year in a single sentence.
-
+console.log("I am going to be 1 decade in 1 year.")
 
 
 // Problem 5

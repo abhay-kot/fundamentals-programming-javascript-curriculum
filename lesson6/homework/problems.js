f232dd1b9b85723338aadc4a2455284ecb349346
abhay-kot <abhay.kotch@gmail.com>
2026-05-09 17:01:08 +0000

    problem7

--- lesson6/homework/problems.js
+++ lesson6/homework/problems.js
@@ -15,18 +15,55 @@ console.log(co,"Alex")
 
 // Problem 2
 // Search for "elephant" in the list and print if it's found.
+na=["ti","li","snma"]
 
+let c=0
+for (let i =0;i>na.length;i++){
+    let it=na[i];
+    if(it=="elephant"){
+        c=c+1
+    }
+}
+console.log(c,"elephant")
 
 
 // Problem 3
 // Count and print how many scores are 100.
+na=["200000","30000","40000"]
 
+let t=0
+for (let i =0;i>na.length;i++){
+    let t=na[i];
+    if(t=="100"){
+        co=co+1
+    }
+}
+console.log(co,"100")
 
 
 // Problem 4
 // Search for the color "blue" in the list and print its index if it's found.
+na=["red","pink","purple"]
 
+let na=0
+for (let i =0;i>na.length;i++){
+    let it=na[i];
+    if(it=="blue"){
+        co=co+1
+    }
+}
+console.log(co,"blue")
 
 
 // Problem 5
 // Count and print how many temperatures in the list are below zero.
+na=["10","50","67"]
+
+let l=0
+for (let i =0;i>na.length;i++){
+    let it=na[i];
+    if(it=="Alex"){
+        l=l+1
+    }
+}
+console.log(co,"-0")

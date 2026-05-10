f2c496bb36cb0fa18b7aff2650f8a45fa88da5e0
abhay-kot <abhay.kotch@gmail.com>
2026-05-10 23:47:22 +0000

    FIxed now

--- lesson7/homework/problems.js
+++ lesson7/homework/problems.js
@@ -1,36 +1,35 @@
 // Problem 1
 // Find and print the sum of all the numbers greater than 25 in the list.
-let num =[1,12,10,6,56785465433,67]
-let big = num[0]; 
-for(let i = 0;i <num.length;i++){
-    let it = num[i]
-    if (it>big){
-        big = it
+let l = [1,5,300,50,25]
+let sum = 0
+for (let i = 0;i<l.length;i++){
+    if (l[i]>25){
+        sum = sum + l[i]
     }
 }
-console.log ("the item that is bigger than 25 is", big)
+
+    
 
 
 // Problem 2
 // Find and print the sum of all the numbers less than -10 in the list.
-let nu =[1,12,67,677000,-56,-44-77]
-let bg = nu[0]; 
-for(let i = -10;i >nu.length;){
-    let it = nu[i]
-    if (it>bg){
-        bg = it
+
+let k = [-1,-5,-3,-5,-25,-30]
+let s = 0
+for (let i = 0;i<k.length;i++){
+    if (k[i]<-10){
+        s = s + k[i]
     }
 }
-console.log ("the biggest item is", bg)
-
+console.log("the sum of the list less than -10 is",s)
 
 // Problem 3
 // Find and print the biggest number less than 100 in the list.
-let nm =[1,12,67,6,99,777]
+let nm =[1,12,67,6,99,777,555,5]
 let b = nm[0]; 
 for(let i = 0;i <nm.length;i++){
     let it = nm[i]
-    if (it>b){
+    if (it<100 ){
         b = it
     }
 }
@@ -39,7 +38,7 @@ console.log ("the biggest item is less than a hundred is", b)
 
 // Problem 4
 // Find and print the biggest number in the list.
-let n =[1,12,67,677000,56785465433]
+let n =[1,12,67,677000,56785465433,66]
 let bi = n[0]; 
 for(let i = 0;i <n.length;i++){
     let it = n[i]
@@ -52,6 +51,10 @@ console.log ("the biggest item is", bi)
 
 // Problem 5
 // Find and print the total sum of all the numbers in the list.
-let i =[60]
-let v = [40]
-console.log("The total sum is",v+i)
+
+let lm = [1,5,30,50,25,30]
+let sm = 0
+for (let i = 0;i<lm.length;i++){
+    sm = sm + lm[i]
+    }
+console.log("Total sum is",sm) 

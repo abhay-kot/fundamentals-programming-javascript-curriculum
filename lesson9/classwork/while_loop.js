a756351ab6e485e0dbedeb2abfb3dc6ae10bfa54
abhay-kot <abhay.kotch@gmail.com>
2026-05-18 01:01:31 +0000

    While loop

---
+++ lesson9/classwork/while_loop.js
@@ -0,0 +1,10 @@
+let i = 1
+while(i<5){
+    console.log("hello 67")
+    i = i +1//have to do or it will go infinitely 
+}
+let k = 1
+while (k<11){
+    console.log(k)
+    k = k + 1
+}
--- lesson9/homework/problems.js
+++ lesson9/homework/problems.js
@@ -1,24 +1,41 @@
 // Problem 1
 // Use a while loop to print the word "JavaScript" 4 times.
-
+let i = 1
+while(i < 5){
+    console.log("Java script")
+    i = i +1 
+}
 
 
 // Problem 2
 // Use a while loop to print the even numbers from 2 to 12 (inclusive).
-
+let y 
+while ( y<13){
+    console.log(y)
+    y = y + 1
+}
 
 
 // Problem 3
 // Ask the user to input a positive number.
 // Use a while loop to count up from 0 to that number (inclusive), printing each number.
+const prompt = require ("prompt-sync")();
 
+let a = Number(prompt("Enter a number that is positive"));
+let b = 0
+while(b <= a){
+    console.log(b)
+    b = b + 1
+}
 
 
 // Problem 4
 // Ask the user to enter a starting number greater than 10.
 // Use a while loop to count down by 5 each time until the number is less than 0.
-
-
+let s = Number(prompt("Enter a number greater than  10"))
+while (s - 5 /s ) {
+    
+}
 
 // Problem 5
 // Create a list of your three favorite animals.
